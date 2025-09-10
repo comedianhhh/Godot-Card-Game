bbc2c724b37705628b9457d791948c622fb9e192
Alan <linianzhi06.com>
2025-09-10 14:37:57 -0400

    Implement campfire healing and UI improvements
    Added a Campfire class with healing functionality and fade-out animation. Updated campfire scene with new UI, particle effects, and animation logic. Campfire now heals the character by 30% max HP and transitions out with animation. Refactored run scene to pass character stats to campfire and improved map and player handler logic for better event handling and visibility checks. Debug buttons in run scene are now hidden by default.

--- SlayTheGodot/scenes/campfire/campfire.gd
+++ SlayTheGodot/scenes/campfire/campfire.gd
@@ -1,5 +1,13 @@
+class_name Campfire
 extends Control
 
+@export var char_stats: CharacterStats
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _on_button_pressed():
+func _on_rest_button_pressed() -> void:
+	char_stats.heal(ceili(char_stats.max_health*0.3))
+	animation_player.play("fade_out")
+	
+# This is called from AnimationPlayer at the end of the fade_out animation
+func _on_fade_out_finnished() ->void:
 	Events.campfire_exited.emit()
